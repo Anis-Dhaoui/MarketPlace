edbb70fef7b1dbebb2d188b601678174327a6c39
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-15 22:27:15 +0100

    Setting up for production on Render

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Main from './Components/MainCmp';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { appStore } from './Redux/Store';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const store = appStore();
 
 class App extends Component {
 
-  render() {
+  render(){
     return (
       <Provider store={store}>
-        <GoogleOAuthProvider clientId='298984122139-49vss2p28pvntciijhscfqcn58ojfr6e.apps.googleusercontent.com'>
-          <BrowserRouter>
-            <div className="App">
-              <Main />
-            </div>
-          </BrowserRouter>
-        </GoogleOAuthProvider>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
