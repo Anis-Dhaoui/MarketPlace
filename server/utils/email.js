cd9704b748f441a6ac7f6328d102669b6b3100d4
Anis Dhaoui <freeconcept6@gmail.com>
2023-05-24 22:23:53 +0100

    Update sending email with using Sendinblue(Brevo)

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,16 +1,33 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+const sendinblueTransport = require('nodemailer-sendinblue-transport');
+
+// const transporter = nodemailer.createTransport(
+//   sendinblueTransport({
+//     apiKey: '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'
+//   })
+// );
+
+// const mailOptions = {
+//   from: 'your-email@example.com',
+//   to: 'recipient@example.com',
+//   subject: 'Hello from Node.js',
+//   text: 'This is a test email sent from Node.js using SendinBlue.'
+// };
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     console.log('Error:', error);
+//   } else {
+//     console.log('Email sent:', info.response);
+//   }
+// });
 
 const sendEmail = (email, subject, message) => {
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      port: 587,
-      secure: true,
-      auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_PWD,
-      },
-    });
+  const transporter = nodemailer.createTransport(
+    sendinblueTransport({
+      apiKey: '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'
+    })
+  );
 
     transporter.sendMail({
       from: 'The Way Shop',
